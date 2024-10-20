0e36d2574774a58be1d283086affcc8309ea9dce
M2004GV <119077902+M2004GV@users.noreply.github.com>
2024-10-20 15:54:02 -0300

    Update gdp.py

--- gdp.py
+++ gdp.py
@@ -11,14 +11,16 @@ df_gdp['Year'] = df_gdp['Year'].apply(lambda x: int(x.split('/')[-1]))
 #verificar os nomes das colunas(podem conter espacos e dificultar na leitura)
 # print(df_gdp.columns)
 
-#split de só que agora para tirar o espaço em branco 
-#e replace pois o valores acima dos milhares são representados por vigula no states
-#troca a virgula por
+#split, só que agora para tirar o espaço em branco 
+#e replace pois os valores acima dos milhares são representados por vírgula no sistema americano
+#troca a virgula por ''
 df_gdp['gdp_pp'] = df_gdp[' GDP_pp '].apply(lambda x: float(x.split()[0].replace(',','')))
 
+
+#deleta uma series coluna do DataFrame
 del df_gdp[' GDP_pp ']
 
-#agora o dataFrame(tabela) contém na coluna year apenas o valor do ano
+#agora o dataFrame(tabela) contém na coluna 'Year' apenas o valor do ano
 #e a parte do gdp.py possui os valores float
 
 #Agora com os dados tratados pode se retirar perguntas do gdp per capita de 1901 a 2011
@@ -35,12 +37,11 @@ df_gdp.groupby('Country')['Year'].min().value_counts()
 df_gdp.groupby('Country')['Year'].min()[df_gdp.groupby('Country')['Year'].min() == 1991]
 
 
-df_gdp[df_gdp['Year'] < 2000].max()
-
 #primeiro ano coletado
 df_gdp_start = df_gdp[df_gdp['Year'] == 1901]
 
 #ultimo ano antes da virado de século
+df_gdp[df_gdp['Year'] < 2000].max()
 df_gdp_end = df_gdp[df_gdp['Year'] == 1996]
 
 # Organizado por crescimento das regiões do menor para o maior gdp_pp
@@ -52,7 +53,7 @@ arr_year = np.arange(df_gdp['Year'].min(), df_gdp['Year'].max())
 df_all_years = pd.DataFrame(arr_year, columns=['Year'])
 df_all_years.index = df_all_years['Year']
 
-
+#verificar os anos ausentes na continuidade
 df_years_off = ~df_all_years['Year'].isin(df_gdp['Year'])
 df_years_off = df_all_years.loc[df_years_off].index
 
@@ -68,6 +69,7 @@ del df_gdp['delta_gdp'], df_gdp['delta_year']
 
 df_new_data = pd.DataFrame()
 
+#iterar e ordenar
 for idx, row in df_gdp.iterrows():
     if row['Year'] == 2011:
         continue
